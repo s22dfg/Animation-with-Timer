1620c2580732a0e9b75d28c7ee22f611504a0c9b
D7703_19 <d7703_19@D7703-19ui-iMac.local>
2018-04-05 11:19:55 +0900

    1~5----5~1

--- Animation with Timer/ViewController.swift
+++ Animation with Timer/ViewController.swift
@@ -12,12 +12,14 @@ class ViewController: UIViewController {
 
     var counter = 1
     var myTimer = Timer()
+    var direction = true
     
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var imageCounter: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 카운터 숫자 표시
         imageCounter.text = String(counter)
     }
 
@@ -27,19 +29,39 @@ class ViewController: UIViewController {
     }
 
     @IBAction func start(_ sender: Any) {
-       myTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+       // 타이머 시간 설정 - 애니메이션
+       myTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
     }
     
     @IBAction func stop(_ sender: Any) {
+        // 타이머 종료
          myTimer.invalidate()
     }
     
     @objc func doAnimation() {
+        
+        // 1부터 5까지 ~ 5부터 1까지
+        if counter == 5 {
+            direction = false
+        } else if counter == 1 {
+            direction = true
+        }
+        
+        if direction == true {
+            counter += 1
+        } else if direction == false {
+            counter = counter - 1
+        }
+        
+        // 1부터 5까지 ~ 다시 1부터 5까지
+        
+        /*
         if counter == 5 {
             counter = 1
         } else {
             counter = counter + 1
         }
+        */
         
         myImageView.image = UIImage(named: "frame\(counter).png")
         imageCounter.text = String(counter)
