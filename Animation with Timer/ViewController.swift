8fb8a7bac81110b9549f1c6beaf652ce32fc5c34
D7703_19 <d7703_19@D7703-19ui-iMac.local>
2018-04-05 11:28:46 +0900

    한 개의 버튼으로 시작과 멈춤을 실행 one button --- start/stop

--- Animation with Timer/ViewController.swift
+++ Animation with Timer/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // 숫자는 1부터 시작 - 레이블 표시 숫자
     var counter = 1
+    
+    // 타이머 선언
     var myTimer = Timer()
+    
+    // 1부터 5까지 가는데 boolean 선언
     var direction = true
+    
+    // 한 개의 버튼으로 시작 멈춤을 하는데 있어서 필요한 boolean선언
+    var button = true
     
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var imageCounter: UILabel!
@@ -30,12 +38,20 @@ class ViewController: UIViewController {
 
     @IBAction func start(_ sender: Any) {
        // 타이머 시간 설정 - 애니메이션
-       myTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+       // 버튼 한 개로 시작과 멈춤을 실행하기
+        if button == true {
+            myTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+            button = false
+        }
+        else if button == false {
+            myTimer.invalidate()
+            button = true
+        }
     }
     
     @IBAction func stop(_ sender: Any) {
         // 타이머 종료
-         myTimer.invalidate()
+        //myTimer.invalidate()
     }
     
     @objc func doAnimation() {
@@ -66,7 +82,5 @@ class ViewController: UIViewController {
         myImageView.image = UIImage(named: "frame\(counter).png")
         imageCounter.text = String(counter)
     }
-    
-
 }
 
